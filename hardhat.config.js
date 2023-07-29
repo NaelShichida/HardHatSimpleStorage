f5af53299bffe0736dce51408abb427b5134f3f7
NaelShichida <nael.shichida2@gmail.com>
2023-07-29 20:38:26 +0100

    Deployment onto sepolia using hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia : {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainID: 58008
+    }
+
+  },
   solidity: "0.8.8",
 };
